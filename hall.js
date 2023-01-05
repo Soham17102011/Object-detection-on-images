10ba18496e1ff5befd5e5166a7de34b49a40fcab
Soham17102011 <115869174+Soham17102011@users.noreply.github.com>
2023-01-05 16:46:29 +0000

    Add files via upload

---
+++ hall.js
@@ -0,0 +1,47 @@
+status = "";
+objects = [];
+img = "";
+
+function preload() {
+    img = loadImage("hall.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!!!!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 380, 380);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+
+            fill("#FF0000")
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            noFill();
+
+        }
+    }
+}
